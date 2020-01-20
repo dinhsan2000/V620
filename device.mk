f3a4598f53296518ffe99c1be11c8febc0045fea
thinhx2 <nthinh1102@gmail.com>
2020-01-20 19:51:15 +0700

    import livedisplay

--- device.mk
+++ device.mk
@@ -28,7 +28,9 @@ PRODUCT_PACKAGES += \
     init.target.rc
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/vendor_fstab.qcom:$(TARGET_COPY_OUT_SYSTEM)/etc/vendor_fstab.qcom
+    $(LOCAL_PATH)/configs/vendor_fstab.qcom:$(TARGET_COPY_OUT_SYSTEM)/etc/vendor_fstab.qcom \
+    $(LOCAL_PATH)/configs/libsdm-disp-apis.qti.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libsdm-disp-apis.qti.so \
+    $(LOCAL_PATH)/configs/vendor.display.color@1.0.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/vendor.display.color@1.0.so
 
 # Input
 PRODUCT_COPY_FILES += \
@@ -45,6 +47,10 @@ PRODUCT_PACKAGES += \
 # Parts
 PRODUCT_PACKAGES += \
     V620AParts
+
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    mokee.livedisplay@2.0-service-V620A
 
 # Inherit from sm6150-common
 $(call inherit-product, device/vsmart/sm6150-common/common.mk)
